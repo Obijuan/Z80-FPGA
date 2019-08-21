b6a64a4d5262bea9b67b9681e978d4e3ef2fbefd
obijuan <juan@iearobotics.com>
2019-08-21 17:33:56 +0200

    Example of reading a varible in memory. It works OK

---
+++ roms/02-read-var.s
@@ -0,0 +1,21 @@
+;-- Ejemplo 2: Lectura de una variable almacenada en memoria
+;-- Se lee la variable var (con direccionamiento directo)
+;-- y se saca por el puerto de LEDs
+
+;-- Puerto de los LEDs
+PORT_LEDS: equ 0x40
+
+
+    org 0x0000
+
+    ;-- Leer la variable var
+    ld	A, (var)
+
+    ;-- Sacar su valor por los LEDs
+  	out	(PORT_LEDS), A
+
+    ;-- Terminar
+    halt
+
+;----- Variable var. Inicializada con un valor
+var: db 0xF0
