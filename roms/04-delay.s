c706e823d23213d5269af736e48b8ed0be7ff04b
obijuan <juan@iearobotics.com>
2019-09-01 20:05:35 +0200

    Example 04 tested on the z80-16KB-boot. OK

--- roms/04-delay.s
+++ roms/04-delay.s
@@ -1,18 +1,20 @@
-;-- Ejemplo 4: Subrutina de delay de 100ms
-;--- Se saca un valor por los LEDs, se esperan 100ms,
+;-- Ejemplo 4: Subrutina de delay
+;--- Se saca un valor por los LEDs, se espera,
 ;--- se saca un segundo valor y se termina
+
+STACK: equ 0x3FFF
 
   org 0x0000
 
   ;-- Configurar la pila
-  ld sp, 0x9FFF
+  ld sp, STACK
 
     ;-- Sacar un valor
     ld	A,0xF0
     out (0x40),A
 
-    ;--- Esperar 100ms
-    call delay_100ms
+    ;--- Esperar
+    call delay
 
     ;-- Sacar otro valor
     ld A,0x0F
@@ -22,17 +24,17 @@
     halt
 
   ;-------------------------------------
-  ;--- Subrutina de Delay de 100ms
-  ;--- El tiempo exacto son 100.044ms
+  ;--- Subrutina de Delay
+  ;--- TODO: Medir el tiempo exacto
   ;------------------------------------
-  delay_100ms:
+  delay:
        ;-- Guardar los registro en la pila
        push af
        push de
        push bc
 
        ld E,0xFF
-  J1:  ld B,0xB3
+  J1:  ld B,0xFF
   J2:  djnz J2
        dec E
        nop
