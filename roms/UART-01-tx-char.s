d7636111ca4ab1e0047fffdf8d460e037c09667e
obijuan <juan@iearobotics.com>
2019-09-04 17:09:35 +0200

    UART-01 test added: sending one character to the PC

---
+++ roms/UART-01-tx-char.s
@@ -0,0 +1,30 @@
+;-- Ejemplo 10: Prueba de envio de un caracter por la UART, directamente
+;-- sin consultar el bit de Ready
+;-- Al abrir el terminal serie se entrara en modo bootloader. Pulsar SW1 para
+;-- hacer un reset, salir del bootloader y ejecutar el porgrama que se haya
+;-- cargado previamente
+
+
+;-- PILA
+STACK: equ 0x3FFF
+
+;---- PUERTOS
+LEDS:  equ 0x40
+SERIAL_TX: equ 0x10
+
+;--- CONSTANTES
+;-- Caracter a enviar por el pueto serie
+CAR: EQU 'A'
+
+  ;--- Comienzo del programa
+  org 0x0000
+
+  ;-- Sacar el caracter por los LEDs
+  ld A, CAR
+  out (LEDS), A
+
+  ;-- Enviarlo por el puerto Serie al PC
+  out (SERIAL_TX), A
+
+  ;-- Terminar
+  halt
