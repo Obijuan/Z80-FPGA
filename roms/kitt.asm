92dd7e41ff24f81934882964c50756c95410b8df
Democrito <epicureo_a@hotmail.com>
2019-10-05 15:32:17 +0200

    KITT example.

---
+++ roms/kitt.asm
@@ -0,0 +1,36 @@
+
+org 0x0000                 ; El programa arranca en la dirección 0 de la ROM
+
+           ld SP,0x3FFF    ; Situa la pila al final de la memoria RAM de la Alhambra-II.
+                           ; La pila nos permite hacer llamadas tipo CALL().
+; Sacamos un 1 por el puerto 0x40 para encender el led0 de la Alhambra-II
+           ld   A, 0x01    ; Carga el registro A con el valor de 1
+           out (0x40),A    ; Saca el valor del registro A por el puerto 0x40 (Leds de la Alhambra).
+
+; Rotamos el bit encendido a la izquierda.
+ROTIZQ:    call DELAY
+           sla A           ; Desplaza los bits del registro A a la izquierda una posición.
+           out (0x40),A    ; Saca el valor del registro A por el puerto 0x40 (Leds de la Alhambra).
+
+           cp 0X80         ; Compara el registro A con el valor 0x80.
+           jr nz, ROTIZQ   ; Si el flag Z no es 1 salta a ROTIZQ.
+           
+; Rotamos el bit encendido a la derecha.          
+ROTDER:    call DELAY
+           srl A           ; Desplaza los bits del registro A a la derecha  una posición.
+           out (0x40),A    ; Saca el valor del registro A por el puerto 0x40 (Leds de la Alhambra).
+           
+           cp 0x01         ; Compara el registro A con el valor 0x01.
+           jr nz, ROTDER   ; Si el flag Z no es 1 salta a ROTDER.
+           
+; Volvemos al repetir la secuencia (bucle infinito)
+JP ROTIZQ
+
+; Timer de aproximadamente 60ms para los 12MHz de la Alhambra
+DELAY:
+J50:       ld E,0xFF       ; Cargamos el registro E a su valor máximo.
+J51:       ld B,0xFF       ; Cargamos el registro B a su valor máximo.
+J52:       djnz J52        ; Decrementa B y mientras no sea 0 saltar a J52.
+           dec E           ; Decrementa E.
+           jr nz,J51       ; Si el flag Z no es 1 salta a J51.
+           ret
